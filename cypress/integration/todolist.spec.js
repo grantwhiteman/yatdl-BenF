421adeff2987054a3f53e179662fc90f9bd54592
bferenczy0 <bferenczy@gmail.com>
2021-07-20 16:33:59 +0100

    making it all work

--- cypress/integration/todolist.spec.js
+++ cypress/integration/todolist.spec.js
@@ -1,13 +1,20 @@
 describe("when user visits page", () => {
     it("shows header", () => {
         cy.visit('/')
-        cy.get('#header').should('contain', 'Yet Another ToDo List')
+        cy.get('#header').should('have.text', 'Yet Another ToDo List')
     })
     it("shows user input", () => {
         cy.visit('/')
         cy.get('#input').type('Take bins out')
         cy.get('#submit').click()
         cy.get('#list').should('contain', 'Take bins out')
+    })
+    it("clears list", () => {
+        cy.visit('/')
+        cy.get('#input').type('Take bins out')
+        cy.get('#submit').click()
+        cy.get('#clear').click()
+        cy.get('#list').should('be.empty')
     })
 
 })
--- src/list.js
+++ src/list.js
@@ -1,24 +1,39 @@
-import React from "react";
+import React, {Component} from "react"
 
-class List extends React.Component {
-    constructor () {
-        super()
-        this.list = [];
+class List extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {list : [], item: ''};
     }
+
+    handleChange = (event) => {
+        this.setState({item: event.target.value})
+    }
+
     handleClick = () => {
-        this.list.push('Take bins out')
-        return this.list
+        this.setState({list: this.state.list.concat(this.state.item)})
+    }
+
+    handleClear = () => {
+        this.setState({list: []})
+    }
+
+    strikethrough = (event) => {
+        this.setState({list: this.state.list.map(item => (item === event.target.value)? <del>{item}</del> : item)})
     }
 
     render () {
         return (
             <div>
-                <input type='text' id='list'/>
+                <input type='text' id='input' onChange={this.handleChange}/>
                 <button type='submit' id='submit' onClick={this.handleClick}>Submit</button>
-
+                <button id='clear' onClick={this.handleClear}>Clear</button>
+                <ol id='list'>{this.state.list.map((item, i) => (
+                    <li key={i}><input type='checkbox' value={item} onChange={this.strikethrough}/>{item}</li>
+                ))}</ol>
             </div>
         )
     }
 }
 
-export default List;
+export default List
